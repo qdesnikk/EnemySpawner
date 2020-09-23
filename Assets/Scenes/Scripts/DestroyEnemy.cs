09fd8ac8637e2289c85dbe4902cbb572bea425fb
qdesnik <pivovarov.vasiliy.np@gmail.com>
2020-09-23 23:36:50 +0300

    Начал проект.

---
+++ Assets/Scenes/Scripts/DestroyEnemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Destroy(gameObject, 5f);
+    }
+}
---
+++ Assets/Scenes/Scripts/SpawnEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Transform _points;
+
+    private Transform[] _spawnPoints;
+
+    void Start()
+    {
+        _spawnPoints = new Transform[_points.childCount];
+
+        for (int i = 0; i < _spawnPoints.Length; i++)
+        {
+            _spawnPoints[i] = _points.GetChild(i);
+        }
+
+        StartCoroutine(Spawner());
+    }
+
+    private IEnumerator Spawner()
+    {
+        var delay = new WaitForSeconds(2);
+
+        for (int i = 0; i <= _spawnPoints.Length; i++)
+        {
+            if (i == _spawnPoints.Length)
+            {
+                i = 0;
+            }
+
+            Instantiate(_enemyPrefab, _spawnPoints[i].position, Quaternion.identity);
+            yield return delay;
+        }
+    }
+}
